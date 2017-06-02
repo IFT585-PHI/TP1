ede9a00c0624899963890ce3a953463b85965408
Alexandre Fradette <alexfrad@hotmail.com>
2017-06-02 14:02:05 -0400

    Read an Input file
    Added a function in the main to be able to read an input file

--- Tp1/Program.cs
+++ Tp1/Program.cs
@@ -8,8 +8,39 @@ namespace Tp1
 {
     class Program
     {
+
+        const string INPUTS_FILENAME = "../../inputs.txt";
+        static int BufferSize;
+        static int Delay;
+        static string SourceFileName;
+        static string DestinationFileName;
+
+
         static void Main(string[] args)
         {
+            ReadInputs();
+
+
+        }
+
+        static void ReadInputs()
+        {
+            try
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(INPUTS_FILENAME);
+                BufferSize = int.Parse(file.ReadLine());
+                Delay = int.Parse(file.ReadLine());
+                SourceFileName = file.ReadLine();
+                DestinationFileName = file.ReadLine();
+                Console.WriteLine("BufferSize: " + BufferSize);
+                Console.WriteLine("Delay: " + Delay);
+                Console.WriteLine("SourceFileName: " + SourceFileName);
+                Console.WriteLine("DestinationFileName: " + DestinationFileName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }    
         }
     }
 }
