6f8d208e7e980005eb5d94b16c956a761443c9e7
Alexandre Fradette <alexfrad@hotmail.com>
2017-06-02 15:40:43 -0400

    Inputs are now in struct

--- Tp1/PhysicalSupport.cs
+++ Tp1/PhysicalSupport.cs
@@ -8,5 +8,33 @@ namespace Tp1
 {
     class PhysicalSupport
     {
+        static bool ReadyToCast;
+        static bool DataReceived;
+
+        static string SendingSource;
+        static string ReceiverDestination;
+
+        static void PseudoCode()
+        {
+            if (ReadyToCast)
+            {
+
+            }
+            else if(!DataReceived)
+            {
+
+            }
+            
+        }
+
+        static void fonction1()
+        {
+            /*
+                Move dataToSend to SendingSource
+
+            */
+            ReadyToCast = false;
+        }
+
     }
 }
--- Tp1/Program.cs
+++ Tp1/Program.cs
@@ -9,36 +9,45 @@ namespace Tp1
     class Program
     {
         const string INPUTS_FILENAME = "../../inputs.txt";
-        static int BufferSize;
-        static int Delay;
-        static string SourceFileName;
-        static string DestinationFileName;
+
+        struct Inputs{
+            public int BufferSize;
+            public int Delay;
+            public string SourceFileName;
+            public string DestinationFileName;
+
+            public void ReadInputs()
+            {
+                try
+                {
+                    //Read the inputs for the program
+                    System.IO.StreamReader file = new System.IO.StreamReader(INPUTS_FILENAME);
+                    BufferSize = int.Parse(file.ReadLine());
+                    Delay = int.Parse(file.ReadLine());
+                    SourceFileName = file.ReadLine();
+                    DestinationFileName = file.ReadLine();
+                    Console.WriteLine("BufferSize: " + BufferSize);
+                    Console.WriteLine("Delay: " + Delay);
+                    Console.WriteLine("SourceFileName: " + SourceFileName);
+                    Console.WriteLine("DestinationFileName: " + DestinationFileName);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
 
         static void Main(string[] args)
         {
-            ReadInputs();
+            Inputs inputs = new Inputs();
+            inputs.ReadInputs();
+            
+            
 
         }
 
-        static void ReadInputs()
-        {
-            try
-            {
-                //Read the inputs for the program
-                System.IO.StreamReader file = new System.IO.StreamReader(INPUTS_FILENAME);
-                BufferSize = int.Parse(file.ReadLine());
-                Delay = int.Parse(file.ReadLine());
-                SourceFileName = file.ReadLine();
-                DestinationFileName = file.ReadLine();
-                Console.WriteLine("BufferSize: " + BufferSize);
-                Console.WriteLine("Delay: " + Delay);
-                Console.WriteLine("SourceFileName: " + SourceFileName);
-                Console.WriteLine("DestinationFileName: " + DestinationFileName);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }    
-        }
+        
     }
 }
