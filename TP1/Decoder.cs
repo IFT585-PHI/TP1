0263ef975c80452d8d81087e614684e4a0e36c2f
CGShocky <charles.huard@usherbrooke.ca>
2017-06-02 16:43:01 -0400

    Added decoder for reciever and thread synchronization

---
+++ TP1/Decoder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp1
+{
+    class Decoder
+    {
+        public string Decode(Weft trame, string mode)
+        {
+            return "0";
+        }
+    }
+}
---
+++ TP1/InterThreadSynchronizer.cs
@@ -0,0 +1,52 @@
+﻿namespace Tp1
+{
+    public class InterThreadSynchronizer
+    {
+        private  Weft envoieSource;
+        private  Weft receptionDestionation;
+        private  bool pretEmmetreSource = true;
+        private  bool recuDestination = false;
+
+        
+        public  bool TransferTrameToSupport(Weft trame)
+        {
+            if (pretEmmetreSource){
+                envoieSource = trame;
+                pretEmmetreSource = false;
+                return true;
+            }   else
+            {
+                return false;
+            }
+        }
+
+        public bool TransferTrameToDestination()
+        {
+            if (!pretEmmetreSource && !recuDestination)
+            {
+                receptionDestionation = envoieSource;
+                recuDestination = true;
+                pretEmmetreSource = true;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool ReadyToReadSourceMessage()
+        {
+            return recuDestination;
+        }
+
+        public Weft GetMessageFromSource()
+        {
+            recuDestination = false;
+            return receptionDestionation;
+        }
+
+
+
+    }
+}
