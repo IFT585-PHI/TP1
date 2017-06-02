4eba49f4bd073abef0e2bbdd8c149dd59f8fa16b
Alexandre <alexfrad@hotmail.com>
2017-06-02 15:42:01 -0400

    Update PhysicalSupport.cs

--- Tp1/PhysicalSupport.cs
+++ Tp1/PhysicalSupport.cs
@@ -8,33 +8,6 @@ namespace Tp1
 {
     class PhysicalSupport
     {
-        static bool ReadyToCast;
-        static bool DataReceived;
-
-        static string SendingSource;
-        static string ReceiverDestination;
-
-        static void PseudoCode()
-        {
-            if (ReadyToCast)
-            {
-
-            }
-            else if(!DataReceived)
-            {
-
-            }
-            
-        }
-
-        static void fonction1()
-        {
-            /*
-                Move dataToSend to SendingSource
-
-            */
-            ReadyToCast = false;
-        }
 
     }
 }
