18dfc9ad98fee0ce78e1b5a615e105459ba5abcb
Alexandre <alexfrad@hotmail.com>
2017-06-03 01:26:49 -0400

    Ian last changes

--- Tp1/Hamming.cs
+++ Tp1/Hamming.cs
@@ -1,26 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
+//Author : Iannick Langevin
 
 namespace Tp1
 {
-    public struct Result
-    {
-        public String data;
-        public Boolean isValid;
-    }
-
     public class Hamming
     {
-        public static void Encode(String d)
+        public static String Encode(String data)
         {
             int nbBitCorrector = 0;
             int posBitCorrector = 0;
-
-            String data = "a";
-            List<char> byteList = ToBinaryString(data);
+            List<char> byteList = StringToBinary(data);
             List<int> indexList = new List<int>();
 
             //Add all the bits corrector initialized at 0
@@ -33,28 +26,46 @@ namespace Tp1
 
             } while (posBitCorrector < byteList.Count);
 
-            //Puts the respective value for each bits corrector
+            //Put the respective value for each bits corrector
             foreach (int i in indexList)
             {
                 byteList[i] = CalculateBitCorrectorForPos(byteList, i + 1);
             }
 
-            foreach (char c in byteList)
-            {
-                Console.Write(c);
-            }
-            Console.WriteLine();
-
-            Console.ReadLine();
+            return string.Join(String.Empty, byteList.ToArray());
         }
 
-        public static Result Decode()
+        public static String Decode(String data)
         {
-            Result r;
-            r.data = "a";
-            r.isValid = Validate();
+            List<char> byteList = new List<char>();
 
-            return r;
+            // Shitty name, mea culpa
+            // Key = posBitCorrector, Value : 0 = no error, 1 = error
+            Dictionary<int, int> indexError = new Dictionary<int, int>();
+
+            foreach (char c in data)
+            {
+                byteList.Add(c);
+            }
+
+            // Look if there is a bit corrector error and fix it
+            if (HasBitCorrectorError(byteList, ref indexError))
+            {
+                foreach(KeyValuePair<int, int> v in indexError)
+                {
+                    if (v.Value == 0)
+                        byteList[v.Key] = ((byteList[v.Key] == '0') ? '1' : '0');
+                }
+            }
+
+            // Remove the bit corrector from the binary string.
+            // Need to reverse the dictionnary because indexes die when you remove them from the beginning of a string... #so much time lost here
+            foreach (KeyValuePair<int, int> v in indexError.Reverse())
+            {
+                byteList.RemoveAt(v.Key);
+            }
+
+            return BinaryToString(string.Join(String.Empty, byteList.ToArray()));
         }
 
         private static Boolean Validate()
@@ -62,13 +73,12 @@ namespace Tp1
             return true;
         }
 
-        private static List<char> ToBinaryString(String a)
+        private static List<char> StringToBinary(String data)
         {
             List<char> byteList = new List<char>();
 
-            //ASCII Table goes from 0 to 127, so 7 binaries are sufficient to hold the char value
-            Byte[] b = Encoding.ASCII.GetBytes(a);
-            String binaryString = string.Join("", b.Select(byt => Convert.ToString(byt, 2).PadLeft(7, '0')));
+            Byte[] bytes = Encoding.ASCII.GetBytes(data);
+            String binaryString = string.Join(String.Empty, bytes.Select(b => Convert.ToString(b, 2).PadLeft(7, '0')));
 
             foreach (char c in binaryString)
                 byteList.Add(c);
@@ -76,6 +86,26 @@ namespace Tp1
             return byteList;
         }
 
+        private static String BinaryToString(String data)
+        {
+            List<Byte> byteList = new List<Byte>();
+
+            for (int i = 0; i < data.Length; i += 7)
+            {
+                byteList.Add(Convert.ToByte(data.Substring(i, 7), 2));
+            }
+
+            return  Encoding.ASCII.GetString(byteList.ToArray());
+        }
+
+        /*
+         * https://en.wikipedia.org/wiki/Hamming_code
+         *      0   1   2   3   4   5   6   7   8   9   10  11
+         * P1   X       X       X       X       X       X
+         * P2       X   X           X   X           X   X
+         * P3               X   X   X   X                   X
+         * P4                               X   X   X   X   X
+         */
         private static char CalculateBitCorrectorForPos(List<char> byteList, int noBits)
         {
             int somme = 0;
@@ -83,15 +113,43 @@ namespace Tp1
 
             while (index < byteList.Count)
             {
+                // Calculate adjacents bits
                 for(int i = 0; i < noBits && index + i < byteList.Count; i++)
                 {
-                    somme += (int)Char.GetNumericValue(byteList[index + i]);
+                    // Don't want to add the corrector bit value when we decode...
+                    if(index != noBits - 1)
+                    {
+                        somme += (int)Char.GetNumericValue(byteList[index + i]);
+                    }
                 }
 
+                // Jump to the next segment
                 index += 2 * noBits;
             }
 
             return (somme % 2).ToString()[0];
+        }
+
+        public static Boolean HasBitCorrectorError(List<char> byteList, ref Dictionary<int, int> indexError)
+        {
+            int nbBitCorrector = 0;
+            int posBitCorrector = 0;
+
+            do
+            {
+                // Calculate the bit corrector and validate it against our current data
+                indexError[posBitCorrector] = ((CalculateBitCorrectorForPos(byteList, posBitCorrector + 1) == byteList[posBitCorrector]) ? 0 : 1);
+                nbBitCorrector++;
+                posBitCorrector = (int)(Math.Pow(2, nbBitCorrector)) - 1;
+            } while (posBitCorrector < byteList.Count);
+
+            foreach(KeyValuePair<int, int> v in indexError)
+            {
+                if(v.Value == 1)
+                    return true;
+            }
+
+            return false;
         }
     }
 }
