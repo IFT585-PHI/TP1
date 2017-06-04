53cb056e9fde7fb181e429baada385273c37cb9b
Iangevin <Iannick.Langevin@gmail.com>
2017-06-03 21:37:00 -0400

    Hamming done
    Added util for conversion fonction + add error in binary string

--- Tp1/Hamming.cs
+++ Tp1/Hamming.cs
@@ -9,22 +9,18 @@ namespace Tp1
 {
     public class Hamming
     {
-        public static String Encode(String data)
+        /// <param name="data">string of char</param>
+        /// <returns>binary string</returns> 
+        public static string Encode(string data)
         {
-            int nbBitCorrector = 0;
-            int posBitCorrector = 0;
-            List<char> byteList = StringToBinary(data);
-            List<int> indexList = new List<int>();
+            List<char> byteList = Util.CharToBinary(data);
+            List<int> indexList = FindAllIndexBitCorrector(byteList);
 
-            //Add all the bits corrector initialized at 0
-            do
+            //Insert all bit correctors
+            foreach(int i in indexList)
             {
-                indexList.Add(posBitCorrector);
-                byteList.Insert(posBitCorrector, '0');
-                nbBitCorrector++;
-                posBitCorrector = (int)(Math.Pow(2, nbBitCorrector)) - 1;
-
-            } while (posBitCorrector < byteList.Count);
+                byteList.Insert(i, '0');
+            }
 
             //Put the respective value for each bits corrector
             foreach (int i in indexList)
@@ -32,70 +28,80 @@ namespace Tp1
                 byteList[i] = CalculateBitCorrectorForPos(byteList, i + 1);
             }
 
-            return string.Join(String.Empty, byteList.ToArray());
+            return Util.ListToBinary(byteList);
         }
 
-        public static String Decode(String data)
+        /// <param name="data">binary string</param>
+        /// <returns>string of char</returns> 
+        public static string Decode(string codedData)
         {
             List<char> byteList = new List<char>();
+            byteList.AddRange(codedData);
+            List<int> indexBitCorrector = FindAllIndexBitCorrector(byteList);
 
-            // Shitty name, mea culpa
-            // Key = posBitCorrector, Value : 0 = no error, 1 = error
-            Dictionary<int, int> indexError = new Dictionary<int, int>();
-
-            foreach (char c in data)
+            // Remove the bit corrector from the binary string, starting from the end
+            indexBitCorrector.Reverse();
+            foreach (int i in indexBitCorrector)
             {
-                byteList.Add(c);
+                byteList.RemoveAt(i);
             }
 
-            // Look if there is a bit corrector error and fix it
-            if (HasBitCorrectorError(byteList, ref indexError))
-            {
-                foreach(KeyValuePair<int, int> v in indexError)
-                {
-                    if (v.Value == 0)
-                        byteList[v.Key] = ((byteList[v.Key] == '0') ? '1' : '0');
-                }
-            }
-
-            // Remove the bit corrector from the binary string.
-            // Need to reverse the dictionnary because indexes die when you remove them from the beginning of a string... #so much time lost here
-            foreach (KeyValuePair<int, int> v in indexError.Reverse())
-            {
-                byteList.RemoveAt(v.Key);
-            }
-
-            return BinaryToString(string.Join(String.Empty, byteList.ToArray()));
+            return Util.BinaryToChar(Util.ListToBinary(byteList));
         }
 
-        private static Boolean Validate()
+        /// <param name="data">binary string</param>
+        public static Boolean Validate(string codedData)
         {
+            List<char> byteList = new List<char>();
+            byteList.AddRange(codedData);
+            List<int> indexBitCorrector = FindAllIndexBitCorrector(byteList);
+            List<int> indexBitCorrectorError = FindBitCorrectorError(byteList, indexBitCorrector);
+
+            // First validation
+            // If there is only one bit corrector error, the bit itself is the error
+            if (indexBitCorrectorError.Count == 1)
+            {
+                int indexBadBit = indexBitCorrectorError[0];
+                byteList[indexBadBit] = ((byteList[indexBadBit] == '0') ? '1' : '0');
+            }
+            // If there is more than 2 bits corrector error, add them to get your faulty index
+            else if (indexBitCorrectorError.Count >= 2)
+            {
+                int indexBadBit = indexBitCorrectorError.Sum() + 1;
+                if(indexBadBit > byteList.Count)
+                {
+                    return false;
+                }
+                byteList[indexBadBit] = ((byteList[indexBadBit] == '0') ? '1' : '0');
+            }
+
+            //2e vérif
+            indexBitCorrector = FindAllIndexBitCorrector(byteList);
+            indexBitCorrectorError = FindBitCorrectorError(byteList, indexBitCorrector);
+            // If there is a second error, we can't do anything about it
+            if(indexBitCorrectorError.Count != 0)
+            {
+                return false;
+            }
+
             return true;
         }
 
-        private static List<char> StringToBinary(String data)
+        private static List<int> FindAllIndexBitCorrector(List<char> byteList)
         {
-            List<char> byteList = new List<char>();
+            List<int> result = new List<int>();
 
-            Byte[] bytes = Encoding.ASCII.GetBytes(data);
-            String binaryString = string.Join(String.Empty, bytes.Select(b => Convert.ToString(b, 2).PadLeft(7, '0')));
-
-            foreach (char c in binaryString)
-                byteList.Add(c);
-
-            return byteList;
-        }
-
-        private static String BinaryToString(String data)
-        {
-            List<Byte> byteList = new List<Byte>();
-
-            for (int i = 0; i < data.Length; i += 7)
+            int nbBitCorrector = 0;
+            int posBitCorrector = 0;
+            do
             {
-                byteList.Add(Convert.ToByte(data.Substring(i, 7), 2));
-            }
+                result.Add(posBitCorrector);
+                nbBitCorrector++;
+                posBitCorrector = (int)(Math.Pow(2, nbBitCorrector)) - 1;
 
-            return  Encoding.ASCII.GetString(byteList.ToArray());
+            } while (posBitCorrector <= byteList.Count);
+
+            return result;
         }
 
         /*
@@ -111,14 +117,17 @@ namespace Tp1
             int somme = 0;
             int index = noBits - 1;
 
+           // Console.WriteLine(noBits);
+
             while (index < byteList.Count)
-            {
+            {        
                 // Calculate adjacents bits
-                for(int i = 0; i < noBits && index + i < byteList.Count; i++)
+                for (int i = 0; i < noBits && index + i < byteList.Count; i++)
                 {
-                    // Don't want to add the corrector bit value when we decode...
-                    if(index != noBits - 1)
+                    // Don't want to add the corrector bit value when decoded because they were all initialized at 0
+                    if(index + i != noBits - 1)
                     {
+                        //Console.Write(index + i + " ");
                         somme += (int)Char.GetNumericValue(byteList[index + i]);
                     }
                 }
@@ -127,29 +136,25 @@ namespace Tp1
                 index += 2 * noBits;
             }
 
+            //Console.WriteLine();
+
+            char a = (somme % 2).ToString()[0];
             return (somme % 2).ToString()[0];
         }
 
-        public static Boolean HasBitCorrectorError(List<char> byteList, ref Dictionary<int, int> indexError)
+        private static List<int> FindBitCorrectorError(List<char> byteList, List<int> indexBitCorrector)
         {
-            int nbBitCorrector = 0;
-            int posBitCorrector = 0;
+            List<int> result = new List<int>();
 
-            do
+            foreach (int noBits in indexBitCorrector)
             {
-                // Calculate the bit corrector and validate it against our current data
-                indexError[posBitCorrector] = ((CalculateBitCorrectorForPos(byteList, posBitCorrector + 1) == byteList[posBitCorrector]) ? 0 : 1);
-                nbBitCorrector++;
-                posBitCorrector = (int)(Math.Pow(2, nbBitCorrector)) - 1;
-            } while (posBitCorrector < byteList.Count);
-
-            foreach(KeyValuePair<int, int> v in indexError)
-            {
-                if(v.Value == 1)
-                    return true;
+                if (byteList[noBits] != CalculateBitCorrectorForPos(byteList, noBits + 1))
+                {
+                    result.Add(noBits);
+                }
             }
 
-            return false;
+            return result;
         }
     }
 }
---
+++ Tp1/Util.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Author: Iannick Langevin
+
+namespace Tp1
+{
+    public class Util
+    {
+        ///<summary>
+        ///Convert a string of char into a string of binary
+        ///</summary>
+        public static List<char> CharToBinary(string data)
+        {
+            List<char> result = new List<char>();
+
+            Byte[] bytes = Encoding.ASCII.GetBytes(data);
+            String binaryString = string.Join(string.Empty, bytes.Select(b => Convert.ToString(b, 2).PadLeft(8, '0')));
+            result.AddRange(binaryString);
+
+            return result;
+        }
+
+        ///<summary>
+        ///Convert a string of binary into a string of char
+        ///</summary>
+        public static string BinaryToChar(string data)
+        {
+            List<Byte> byteList = new List<Byte>();
+
+            for (int i = 0; i < data.Length; i += 8)
+            {
+                byteList.Add(Convert.ToByte(data.Substring(i, 8), 2));
+            }
+
+            return Encoding.ASCII.GetString(byteList.ToArray());
+        }
+
+        ///<summary>
+        ///Convert a list of char into a binary string
+        ///</summary>
+        public static string ListToBinary(List<char> list)
+        {
+            return string.Join(string.Empty, list.ToArray());
+        }
+
+        public static void InjectErrorAtPosition(ref string codedData, int pos)
+        {
+            StringBuilder s = new StringBuilder(codedData);
+            s[pos] = ((s[pos] == '0') ? '1' : '0'); ;
+            codedData = s.ToString();
+        }
+
+        public static void InjectErrorRandom(ref string codedData)
+        {
+            Random r = new Random();
+            int ra = r.Next(0, codedData.Length - 1);
+            InjectErrorAtPosition(ref codedData, ra);
+
+            Console.WriteLine(ra);
+            System.Threading.Thread.Sleep(100);
+        }
+    }
+}
